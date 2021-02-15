d1568cba9140e76518e7799848694d5bd9a4b9fa
archanaGit84 <73020472+archanaGit84@users.noreply.github.com>
2021-02-15 12:48:53 +0100

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,50 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    getScore(){
+        var playerScore = "players/player/score" + this.index;
+        var scoreRef = database.ref(playerScore);
+        scoreRef.on("value", (data) => {
+            playerScore = data.val();
+        })
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+    
+}
